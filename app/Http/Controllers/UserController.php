8b5de34ad58c568061c837e4ca5f99a5bc3361ab
greshilov.v <greshilov@a2design.biz>
2020-05-30 23:58:03 +0600

    added: user controller stubs

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller
+{
+    public function withdrawMoney()
+    {
+        dd('Вывод денег: интеграция финансовой API');
+    }
+    public function withdrawCoins()
+    {
+        dd('Вывод внутренней валюты: интеграция какой-то API');
+    }
+}
