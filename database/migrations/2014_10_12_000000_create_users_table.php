7394b100d879eae6a7366d9c60ca8d54b22d6ac1
greshilov.v <greshilov@a2design.biz>
2020-05-30 22:34:00 +0600

    fixed: users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
+            $table->string('password');
+            $table->string('role', 50)->default('user');
             $table->unsignedFloat('coins')->default(0);
             $table->unsignedFloat('money')->default(0);
             $table->rememberToken();
